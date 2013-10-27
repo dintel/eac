b16d6af05fd7d33dadeb02f6b3bfac923154c4f4
Dmitry Zbarski <dmitry.zbarski@gmail.com>
2013-10-27 12:05:07 +0200

    Add script to automate download of test files from S3

---
+++ tests/update_tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FILES="2.raw  3.raw  dima20k.txt  dskool.txt  electrop.txt  goldbug.poe.txt  modem.txt  random128.bin  random16.bin  random1.bin  random32.bin  random64.bin  random8.bin  sp4rpt.txt"
+HOST="dima.z.s3.amazonaws.com"
+
+cd files
+
+for FILE in $FILES; do
+    wget http://$HOST/$FILE
+done
+
+cd ..
