f14f0cf028ad16b5a28d7c669543cdfb6ded2e04
Dmitry Zbarski <dmitry.zbarski@gmail.com>
2013-11-18 12:12:37 +0200

    Update test files list

--- tests/update_tests.sh
+++ tests/update_tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 HOST="dima.z.s3.amazonaws.com"
-FILES="2.raw dima20k.txt dskool.txt electrop.txt entrop_082-32.txt entrop_082-64.txt entrop_max-32.txt goldbug.poe.txt modem.txt random128.bin random16.bin random1.bin random32.bin random64.bin random8.bin sp4rpt.txt"
+FILES="1.raw dima20k.txt dskool.txt electrop.txt entrop_082-32.txt entrop_082-64.txt entrop_max-32.txt goldbug.poe.txt modem.txt random128.bin random16.bin random1.bin random32.bin random64.bin random8.bin sp4rpt.txt"
 
 cd files
 
