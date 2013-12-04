d612098b9a188177ec0a3952a650153336c89096
Dmitry Zbarski <dmitry.zbarski@gmail.com>
2013-12-04 12:53:31 +0200

    Add generator of synthetic data files (closes #1)

--- Makefile
+++ Makefile
@@ -20,20 +20,25 @@ COFLAGS = -c				\
 
 ENCODER = eac_encode
 DECODER = eac_decode
+GENERATOR = generator
 ENTROPY_CALC = entropy_calc
 ENCODER_OBJECTS = encoder.o lz77.o cfc.o bit_string.o bit_string_writer.o log.o queue.o block.o
 DECODER_OBJECTS = decoder.o lz77.o cfc.o bit_string.o bit_string_writer.o log.o
+GENERATOR_OBJECTS = generator.o bit_string.o bit_string_writer.o log.o
 ENTROPY_CALC_OBJECTS = entropy_calc.o log.o
 
 .PHONY: all
 
-all: tags $(ENCODER) $(DECODER)
+all: tags $(ENCODER) $(DECODER) $(GENERATOR) $(ENTROPY_CALC)
 
 $(ENCODER): $(ENCODER_OBJECTS)
 	$(CC) $(ENCODER_OBJECTS) -o $@ $(CFLAGS)
 
 $(DECODER): $(DECODER_OBJECTS)
 	$(CC) $(DECODER_OBJECTS) -o $@ $(CFLAGS)
+
+$(GENERATOR): $(GENERATOR_OBJECTS)
+	$(CC) $(GENERATOR_OBJECTS) -o $@ $(CFLAGS)
 
 $(ENTROPY_CALC): $(ENTROPY_CALC_OBJECTS)
 	$(CC) $(ENTROPY_CALC_OBJECTS) -o $@ $(CFLAGS)
--- generator.c
+++ generator.c
@@ -3,12 +3,13 @@
 #include <stdlib.h>
 #include <argp.h>
 #include <unistd.h>
+#include <time.h>
 #include "bit_string.h"
 #include "bit_string_writer.h"
 #include "log.h"
 
 /** Generator version */
-const char *argp_program_version = "eac_generator 0.1";
+const char *argp_program_version = "eac_generator 0.2";
 /** Email to send generator bug reports */
 const char *argp_program_bug_address = "<dmitry.zbarski@gmail.com>";
 
@@ -20,19 +21,19 @@ static char args_doc[] = " -o OUTPUT_FILE -n SIZE";
 
 /** The options generator accpets */
 static struct argp_option options[] = {
-    {"size",   'n', "FILE",       0, "Size of file in bytes" },
-    {"zero",   '0', "NUM",        0, "Probability of 0" },
-    {"one",    '1', "NUM",        0, "Probability of 1" },
-    {"output", 'o', "FILE",       0, "Output file name" },
+    {"size",        'n', "FILE",       0, "Size of file in bytes" },
+    {"probability", 'p', "NUM",        0, "Probability of 0 (probability of 1 is calculated automatically)" },
+    {"output",      'o', "FILE",       0, "Output file name" },
     { 0 }
 };
-     
+
 /** \brief Generator arguments structure */
 struct generator_arguments {
-    size_t size;         /**< window size parameter */
+    size_t size;         /**< file size parameter */
     char *file;          /**< output file parameter */
+    double probability;  /**< probability of 0 */
 };
-     
+
 /** Parse a single option of generator */
 static error_t generator_parse_opt(int key, char *arg, struct argp_state *state)
 {
@@ -48,6 +49,9 @@ static error_t generator_parse_opt(int key, char *arg, struct argp_state *state)
     case 'n':
         arguments->size = atoi(arg);
         break;
+    case 'p':
+        arguments->probability = atof(arg);
+        break;
      
     case ARGP_KEY_ARG:
         if(state->arg_num >= 1)
@@ -55,7 +59,7 @@ static error_t generator_parse_opt(int key, char *arg, struct argp_state *state)
         break;
      
     case ARGP_KEY_END:
-        if(arguments->output_file == 0)
+        if(arguments->file == 0 || arguments->size <= 0 || arguments->probability < 0 || arguments->probability > 1)
             argp_usage (state);
         break;
      
@@ -70,7 +74,38 @@ static struct argp argp = { options, generator_parse_opt, args_doc, doc };
 
 int main(int argc, char *argv[])
 {
+    struct generator_arguments arguments;
+     
+    /* Default values. */
+    arguments.size = 0;
+    arguments.file = 0;
+    arguments.probability = -1.0;
     
+    /* Parse our arguments; every option seen by enc_parse_opt will
+       be reflected in arguments. */
+    argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+    FILE *file = fopen(arguments.file,"w");
+    if(file == NULL) {
+        error(1, errno, "Could not open input file");
+    }
+
+    bit_string_t *bs = bit_string_init(arguments.size);
+    bit_string_writer_t *writer = bit_string_writer_init(file);
+
+    srand(time(NULL));
+    for(int i = 0; i < arguments.size * 8; i++) {
+        if(rand() >= arguments.probability * RAND_MAX) {
+            bit_string_append_bit(bs,0);
+        } else {
+            bit_string_append_bit(bs,1);
+        }
+    }
+    bit_string_writer_write(writer,bs);
+    bit_string_writer_flush(writer);
+    
+    bit_string_writer_destroy(writer);
+    bit_string_destroy(bs);
     
     return 0;
 }
